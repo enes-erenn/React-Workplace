c45f9cb3b83e669c7c63e6cdf3daad8cddb218e1
Enes Eren <88148105+thenesern@users.noreply.github.com>
2022-01-01 12:30:50 +0300

    Created the "Expenses" Component and UI component called "Card"

--- src/components/expenses/ExpenseItem.js
+++ src/components/expenses/ExpenseItem.js
@@ -1,14 +1,16 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate.js";
+import Card from "./UI/Card.js";
+
 function ExpenseItem(props) {
   return (
-    <div className="expense-item">
-      <ExpenseDate date={props.date}/>
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
--- src/components/expenses/ExpensesApp.js
+++ src/components/expenses/ExpensesApp.js
@@ -1,4 +1,4 @@
-import ExpenseItem from "./ExpenseItem";
+import Expenses from "./Expenses.js";
 
 function ExpensesApp() {
   const expenses = [
@@ -29,26 +29,7 @@ function ExpensesApp() {
   ];
   return (
     <div>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      />
+      <Expenses items={expenses} />
     </div>
   );
 }
