6fe90a3fe3675954c56f28b9076d48b6403e3269
Enes Eren <88148105+thenesern@users.noreply.github.com>
2022-01-01 12:04:41 +0300

    Added "Props" to getting data

--- src/components/expenses/ExpenseItem.js
+++ src/components/expenses/ExpenseItem.js
@@ -1,18 +1,22 @@
-import "./ExpenseItem.css"
+import "./ExpenseItem.css";
 
-function ExpenseItem() {
-    const expenseDate = new Date(2021, 12, 31);
-    const expenseTitle = "Car Insurance";
-    const expenseAmount = 399;
-    return(
-        <div className="expense-item">
-            <div>{expenseDate.toISOString()}</div>
-            <div className="expense-item__description">
-                <h2>{expenseTitle}</h2>
-                <div className="expense-item__price">${expenseAmount}</div>
-            </div>
-        </div>
-    )
-};
+function ExpenseItem(props) {
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
+  return (
+    <div className="expense-item">
+      <div>
+        <div>{month}</div>
+        <div>{day}</div>
+        <div>{year}</div>
+      </div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </div>
+  );
+}
 
 export default ExpenseItem;
--- src/components/expenses/ExpensesApp.js
+++ src/components/expenses/ExpensesApp.js
@@ -1,9 +1,56 @@
 import ExpenseItem from "./ExpenseItem";
 
 function ExpensesApp() {
-    return(
-        <ExpenseItem />
-    )
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 20,
+      date: new Date(2021, 12, 3),
+    },
+    {
+      id: "e2",
+      title: "Car Insurance",
+      amount: 399,
+      date: new Date(2021, 12, 7),
+    },
+    {
+      id: "e3",
+      title: "Monitor",
+      amount: 299,
+      date: new Date(2021, 12, 15),
+    },
+    {
+      id: "e4",
+      title: "Phone",
+      amount: 1099,
+      date: new Date(2021, 12, 20),
+    },
+  ];
+  return (
+    <div>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      />
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      />
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      />
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      />
+    </div>
+  );
 }
 
 export default ExpensesApp;
