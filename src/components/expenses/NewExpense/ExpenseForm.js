573c6d6a684a890aef531b58df619eb4b3f22c47
Enes Eren <88148105+thenesern@users.noreply.github.com>
2022-01-03 13:35:32 +0300

    Bug fix in "ExpenseForm"

--- src/components/expenses/NewExpense/ExpenseForm.js
+++ src/components/expenses/NewExpense/ExpenseForm.js
@@ -23,7 +23,7 @@ function ExpenseForm(props) {
 
     const expenseData = {
         title: enteredTitle,
-        amount: enteredAmount,
+        amount: +enteredAmount,
         date: new Date(enteredDate)
     }
 
