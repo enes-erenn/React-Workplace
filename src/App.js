424592ef7a1e2110e240ee237cbe56731bf45505
Enes Eren <88148105+thenesern@users.noreply.github.com>
2022-01-22 14:48:27 +0300

    General UI Update

--- src/App.js
+++ src/App.js
@@ -1,76 +1,193 @@
 import "./App.css";
 import { Link } from "react-router-dom";
-import { useState } from "react";
 
 function App() {
-  const [onHover, setOnHover] = useState(false);
-
   return (
-    <div className="App">
-      <section>
-        <article>
-          <Link to="/contacts">
-            <div
-              className="sides"
-              onMouseEnter={() => setOnHover(true)}
-              onMouseLeave={() => setOnHover(false)}
-            >
-              {onHover && <p>See on Page</p>}
-            </div>
-          </Link>
-        </article>
+    <section className="App">
+      <article>
+        <Link to="/contacts">
+          <div className="works">
+            <h3>Contacts App</h3>
+            <img
+              class="imgs"
+              src="https://images.pexels.com/photos/4439444/pexels-photo-4439444.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+              alt=""
+            />
+          </div>
+        </Link>
+      </article>
 
-        <article>
-          <Link to="/weatherapp">
-            <div
-              className="sides"
-              onMouseEnter={() => setOnHover(true)}
-              onMouseLeave={() => setOnHover(false)}
-            >
-              {onHover && <p>See on Page</p>}
-            </div>
-          </Link>
-        </article>
-        <article>
-          <Link to="/expensesapp">
-            <div
-              className="sides"
-              onMouseEnter={() => setOnHover(true)}
-              onMouseLeave={() => setOnHover(false)}
-            >
-              {onHover && <p>See on Page</p>}
-            </div>
-          </Link>
-        </article>
-        <article>
-          <div className="sides">4</div>
-        </article>
-        <article>
-          <div className="sides">5</div>
-        </article>
-        <article>
-          <div className="sides">6</div>
-        </article>
-        <article>
-          <div className="sides">7</div>
-        </article>
-        <article>
-          <div className="sides">8</div>
-        </article>
-        <article>
-          <div className="sides">9</div>
-        </article>
-        <article>
-          <div className="sides">10</div>
-        </article>
-        <article>
-          <div className="sides">11</div>
-        </article>
-        <article>
-          <div className="sides">12</div>
-        </article>
-      </section>
-    </div>
+      <article>
+        <Link to="/weatherapp">
+          <div className="works">
+            <h3>Weather App</h3>
+            <img
+              class="imgs"
+              src="https://images.pexels.com/photos/4439444/pexels-photo-4439444.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+              alt=""
+            />
+          </div>
+        </Link>
+      </article>
+
+      <article>
+        <Link to="/expensesapp">
+          <div className="works">
+            <h3>Expenses App</h3>
+            <img
+              class="imgs"
+              src="https://images.pexels.com/photos/4439444/pexels-photo-4439444.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+              alt=""
+            />
+          </div>
+        </Link>
+      </article>
+
+      <article>
+        <Link to="">
+          <div className="works">
+            <h3>4</h3>
+            <img
+              class="imgs"
+              src="https://images.pexels.com/photos/4439444/pexels-photo-4439444.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+              alt=""
+            />
+          </div>
+        </Link>
+      </article>
+      <article>
+        <Link to="">
+          <div className="works">
+            <h3>5</h3>
+            <img
+              class="imgs"
+              src="https://images.pexels.com/photos/4439444/pexels-photo-4439444.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+              alt=""
+            />
+          </div>
+        </Link>
+      </article>
+      <article>
+        <Link to="">
+          <div className="works">
+            <h3>6</h3>
+            <img
+              class="imgs"
+              src="https://images.pexels.com/photos/4439444/pexels-photo-4439444.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+              alt=""
+            />
+          </div>
+        </Link>
+      </article>
+      <article>
+        <Link to="">
+          <div className="works">
+            <h3>7</h3>
+            <img
+              class="imgs"
+              src="https://images.pexels.com/photos/4439444/pexels-photo-4439444.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+              alt=""
+            />
+          </div>
+        </Link>
+      </article>
+      <article>
+        <Link to="">
+          <div className="works">
+            <h3>8</h3>
+            <img
+              class="imgs"
+              src="https://images.pexels.com/photos/4439444/pexels-photo-4439444.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+              alt=""
+            />
+          </div>
+        </Link>
+      </article>
+      <article>
+        <Link to="">
+          <div className="works">
+            <h3>9</h3>
+            <img
+              class="imgs"
+              src="https://images.pexels.com/photos/4439444/pexels-photo-4439444.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+              alt=""
+            />
+          </div>
+        </Link>
+      </article>
+      <article>
+        <Link to="">
+          <div className="works">
+            <h3>10</h3>
+            <img
+              class="imgs"
+              src="https://images.pexels.com/photos/4439444/pexels-photo-4439444.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+              alt=""
+            />
+          </div>
+        </Link>
+      </article>
+      <article>
+        <Link to="">
+          <div className="works">
+            <h3>11</h3>
+            <img
+              class="imgs"
+              src="https://images.pexels.com/photos/4439444/pexels-photo-4439444.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+              alt=""
+            />
+          </div>
+        </Link>
+      </article>
+      <article>
+        <Link to="">
+          <div className="works">
+            <h3>12</h3>
+            <img
+              class="imgs"
+              src="https://images.pexels.com/photos/4439444/pexels-photo-4439444.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+              alt=""
+            />
+          </div>
+        </Link>
+      </article>
+      <article>
+        <Link to="">
+          <div className="works">
+            <h3>13</h3>
+            <img
+              class="imgs"
+              src="https://images.pexels.com/photos/4439444/pexels-photo-4439444.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+              alt=""
+            />
+          </div>
+        </Link>
+      </article>
+      <article>
+        <Link to="">
+          <div className="works">
+            <h3>14</h3>
+            <img
+              class="imgs"
+              src="https://images.pexels.com/photos/4439444/pexels-photo-4439444.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+              alt=""
+            />
+          </div>
+        </Link>
+      </article>
+      <article>
+        <Link to="">
+          <div className="works">
+            <h3>15</h3>
+            <img
+              class="imgs"
+              src="https://images.pexels.com/photos/4439444/pexels-photo-4439444.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+              alt=""
+            />
+          </div>
+        </Link>
+      </article>
+    </section>
   );
 }
 
