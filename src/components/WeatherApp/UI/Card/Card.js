b5f5103dc82b4ea91339f34f343beb91f2527b05
Enes Eren <88148105+thenesern@users.noreply.github.com>
2022-01-24 20:01:59 +0300

    General UI Improvements

--- src/components/WeatherApp/UI/Card/Card.js
+++ src/components/WeatherApp/UI/Card/Card.js
@@ -6,11 +6,11 @@ function WeatherCard(props) {
     <div className={styles.container}>
       <p id={styles.cityName}>{props.weatherCity}</p>
       <div className={styles.box__imgDeg}>
-        <img className={styles.img}src={props.imgUrl} alt="" />
-        <span>{props.weatherDeg}°C</span>
+        <img className={styles.img} src={props.imgUrl} alt="" />
+        <span className={styles.deg}>{props.weatherDeg}°C</span>
       </div>
       <div className={styles.boxDescription}>
-        <span>{props.weatherDes}</span>
+        <span className={styles.des}>{props.weatherDes}</span>
       </div>
     </div>
   );
