c2953b53f3f9ea4988fc0d941c720f002a7d0aa9
Enes Eren <88148105+thenesern@users.noreply.github.com>
2022-02-08 10:11:12 +0300

    Used useRef for Cart

--- src/components/MyMeals/MealItem/MealItemForm.js
+++ src/components/MyMeals/MealItem/MealItemForm.js
@@ -1,11 +1,32 @@
-import React from "react";
+// Packages
+import React, { useRef, useState } from "react";
+// Styles
 import styles from "./MealItemForm.module.css";
+// Components
 import Input from "../UI/Input.js";
 
 const MealItemForm = (props) => {
+  const [amountIsValid, setAmountIsValid] = useState(true);
+  const amountInputRef = useRef();
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNumber = +enteredAmount;
+    if (
+      enteredAmount.trim().length === 0 ||
+      enteredAmount < 1 ||
+      enteredAmount > 5
+    ) {
+      setAmountIsValid(false);
+      return;
+    }
+    props.onAddToCart(enteredAmountNumber);
+  };
+
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={submitHandler}>
       <Input
+        ref={amountInputRef}
         type="text"
         label="Amount"
         input={{
@@ -18,6 +39,7 @@ const MealItemForm = (props) => {
         }}
       />
       <button>+ Add</button>
+      {!amountIsValid && <p>Please enter a valid amount.</p>}
     </form>
   );
 };
--- src/components/MyMeals/UI/Input.js
+++ src/components/MyMeals/UI/Input.js
@@ -1,13 +1,15 @@
+// Packages
 import React from "react";
+// Styles
 import styles from "./Input.module.css";
 
-const Input = (props) => {
+const Input = React.forwardRef((props, ref) => {
   return (
     <div className={styles.input}>
       <label htmlFor={props.input.id}>{props.label}</label>
-      <input {...props.input} />
+      <input ref={ref} {...props.input} />
     </div>
   );
-};
+});
 
 export default Input;
