2069d036f7f0e405c732cadc0b30d13817479c11
Enes Eren <88148105+thenesern@users.noreply.github.com>
2022-02-08 10:09:54 +0300

    MyMeals, Modal Component Created

---
+++ src/components/MyMeals/UI/Modal.js
@@ -0,0 +1,35 @@
+// Packages
+import ReactDOM from "react-dom";
+// Styles
+import styles from "./Modal.module.css";
+
+const Backdrop = (props) => {
+  return <div className={styles.backdrop} onClick={props.onClose}></div>;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={styles.modal}>
+      <div className={styles.content}>{props.children}</div>
+    </div>
+  );
+};
+
+const portalElement = document.getElementById("overlays");
+
+const Modal = (props) => {
+  return (
+    <>
+      {ReactDOM.createPortal(
+        <Backdrop onClose={props.onClose} />,
+        portalElement
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalElement
+      )}
+    </>
+  );
+};
+
+export default Modal;
