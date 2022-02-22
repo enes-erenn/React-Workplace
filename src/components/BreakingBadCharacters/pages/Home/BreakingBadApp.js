e276c8ded2c409dc2e27378f9a849e7e728e016d
Enes Eren <88148105+thenesern@users.noreply.github.com>
2022-02-22 12:01:39 +0300

    Characters Fetched and Rendered

--- src/components/BreakingBadCharacters/pages/Home/BreakingBadApp.js
+++ src/components/BreakingBadCharacters/pages/Home/BreakingBadApp.js
@@ -1,10 +1,42 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchCharacters } from "../../../../store/CharactersSlice/CharactersSlice.js";
+import styles from "./BreakingBadApp.module.css";
 
 const Home = () => {
-  const data = useSelector((state) => state);
-  console.log(data);
-  return <div>Home</div>;
+  const characters = useSelector((state) => state.characters.items);
+  const isLoading = useSelector((state) => state.characters.isLoading);
+  const error = useSelector((state) => state.characters.error);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCharacters());
+  }, [dispatch]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div>{error}</div>;
+  }
+  return (
+    <div className={styles.container}>
+      <h1>Characters</h1>
+
+      <div className={styles.characters}>
+        {characters.map((character) => (
+          <div key={character.char_id}>
+            <img
+              className={styles.image}
+              alt={character.name}
+              src={character.img}
+            ></img>
+            <p>{character.name}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- src/store/CharactersSlice/CharactersSlice.js
+++ src/store/CharactersSlice/CharactersSlice.js
@@ -1,11 +1,37 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const char_limit = 12;
+export const fetchCharacters = createAsyncThunk(
+  "characters/getCharacters",
+  async () => {
+    const res = await axios(
+      `${process.env.REACT_APP_API_BASE_ENDPOINT}/characters?limit=${char_limit}`
+    );
+    return res.data;
+  }
+);
 
 export const CharactersSlice = createSlice({
   name: "characters",
   initialState: {
     items: [],
+    isLoading: false,
   },
   reducers: {},
+  extraReducers: {
+    [fetchCharacters.pending]: (state, action) => {
+      state.isLoading = true;
+    },
+    [fetchCharacters.fulfilled]: (state, action) => {
+      state.items = action.payload;
+      state.isLoading = false;
+    },
+    [fetchCharacters.rejected]: (state, action) => {
+      state.isLoading = false;
+      state.error = action.error.message;
+    },
+  },
 });
 
 export default CharactersSlice.reducer;
